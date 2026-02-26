6c89a78bae19bb686e5ac5bc5adcd988962446f4
kaanc3tin <kaan_1536@icloud.com>
2026-02-26 13:17:47 +0300

    update Theme to use LightColorScheme for dark mode, remove unused DarkColorScheme definition 🌗

--- app/src/main/java/com/karegraf/androidarchitecturestarterkit/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/karegraf/androidarchitecturestarterkit/presentation/ui/theme/Theme.kt
@@ -10,11 +10,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -35,7 +30,7 @@ fun AndroidArchitectureStarterKitTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        darkTheme -> LightColorScheme
         else -> LightColorScheme
     }
 
